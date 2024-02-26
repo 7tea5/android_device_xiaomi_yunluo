a2e3a73dea4e378205321b7fd8423c2066ef6ca8
citrine <111177829+ci-r@users.noreply.github.com>
2024-02-26 15:51:01 +0530

    yunluo: Add Android Go flag
    Change-Id: I22d7e2a69f9a04f6920f1925a45d15bed19e323e
    Signed-off-by: citrine <111177829+ci-r@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
 # GMS
 WITH_GMS_COMMS_SUITE := false
 
+# AndroidGo Flags
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Keymaster / Keymint
 PRODUCT_PACKAGES += \
 	libkeymaster_messages.vendor:64 \
