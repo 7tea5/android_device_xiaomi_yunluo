3d5a7a99a78e4239251bdfc5b3c13b83ab92127e
citrine <111177829+ci-r@users.noreply.github.com>
2024-02-27 12:58:25 +0530

    yunluo: dex2oat: strip some debug packages
    Change-Id: Icad0acdd29e0426221b9774737534f24a0e2a7ed
    Signed-off-by: citrine <111177829+ci-r@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -180,6 +180,12 @@ WITH_GMS := true
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+USE_DEX2OAT_DEBUG := false
+WITH_DEXPREOPT_DEBUG_INFO := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Keymaster / Keymint
 PRODUCT_PACKAGES += \
 	libkeymaster_messages.vendor:64 \
